34dddcd2b75618046410140f95f53861c91f580d
guxiaona <15262526680@139.com>
2021-07-19 16:54:27 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
   <div id="app">
+    <h2>--------modules中的内容--------</h2>
+    <h2>{{ $store.state.a.name }}</h2>
+    <button @click="updateName">修改名字</button>
+    <h2>{{ $store.getters.fullname }}</h2>
+    <h2>{{ $store.getters.fullname2 }}</h2>
+    <h2>{{ $store.getters.fullname3 }}</h2>
+    <button @click="asycUpdataName">异步修改</button>
+
+    <h2>--------app中的内容--------</h2>
     <h2>{{ message }}</h2>
-    <h2>{{ counter }}</h2>
-    <button @click="counter++">+</button>
-    <button @click="counter--">-</button>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="addition">+</button>
+    <button @click="subtraction">-</button>
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStudent">添加学生</button>
+
+    <h2>-------------getters相关信息-------</h2>
+    <h2>{{ $store.getters.powerCounter }}</h2>
+    <h2>{{ $store.getters.more20stu }}</h2>
+    <h2>{{ $store.getters.more20stuLength }}</h2>
+    <h2>{{ $store.getters.moreAgeStu(30) }}</h2>
+
+    <h2>-------------info对象的内容是否是响应式的------</h2>
+    <h2>{{ $store.state.info }}</h2>
+    <button @click="updateInfo">修改信息</button>
+
+    <h2>-------------hellovuex内容------</h2>
     <hello-vuex :counter="counter"></hello-vuex>
   </div>
 </template>
 
 <script>
 import HelloVuex from "./components/HelloVuex.vue";
+import { INCREMENT } from "./store/mutations.types";
+
 export default {
   name: "App",
   data() {
@@ -20,6 +46,52 @@ export default {
   },
   components: {
     HelloVuex,
+  },
+  methods: {
+    addition() {
+      this.$store.commit("INCREMENT");
+    },
+    subtraction() {
+      this.$store.commit("decrement");
+    },
+    addCount(count) {
+      // 负载payload 参数或者对象
+      // 1.普通的提交封装
+      this.$store.commit("incrementCount", count);
+
+      // 2.特殊的提交封装 count 就变成了一个负载对象
+      // this.$store.commit({
+      //   type: "incrementCount",
+      //   count,
+      // });
+    },
+    addStudent() {
+      const stu = { id: 5, name: "why5", age: 44 };
+      this.$store.commit("addStudent", stu);
+    },
+    updateInfo() {
+      // this.$store.commit("updateInfo");
+
+      // 方法一
+      // this.$store.dispatch("aUpdateInfo", {
+      //   message: "我是携带的信息",
+      //   success: () => {
+      //     console.log("里面已经完成了");
+      //   },
+      // });
+
+      // 方法二
+      this.$store.dispatch("aUpdateInfo", "我是携带的信息").then((res) => {
+        console.log("里面已经完成了");
+        console.log(res);
+      });
+    },
+    updateName() {
+      this.$store.commit("updateName", "lisi");
+    },
+    asycUpdataName() {
+      this.$store.dispatch("aUpdateName");
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,134 @@
 import { createStore } from 'vuex'
+import { INCREMENT } from "./mutations.types";
+
+// 1.安装插件
+
+// 2.创建对象
+const moduleA = {
+    state: {
+        name: '张三'
+    },
+    mutations: {
+        // updateName(state) {
+        //     state.name = 'lisi'
+        // }
+        updateName(state, payload) {
+            state.name = payload
+        }
+    },
+    getters: {
+        fullname(state) {
+            return state.name + '111'
+        },
+        fullname2(state, getters) {
+            return getters.fullname + '222'
+        },
+        fullname3(state, getters, rootstate) {
+            return getters.fullname2 + rootstate.counter
+        }
+    },
+    actions: {
+        aUpdateName(context) {
+            // 只会调用当前模块中的
+            console.log(context);
+            setTimeout(() => {
+                context.commit('updateName', 'wangwu')
+            }, 1000)
+        }
+    },
+
+}
 
 export default createStore({
     state: {
-        counter: 1000
+        counter: 1000,
+        students: [
+            { id: 1, name: 'why1', age: 13 },
+            { id: 2, name: 'why2', age: 23 },
+            { id: 3, name: 'why3', age: 33 },
+            { id: 4, name: 'why4', age: 18 },
+        ],
+        info: {
+            name: 'kobe',
+            age: 43,
+            height: 198
+        }
     },
-    mutations: {},
-    actions: {},
-    getters: {},
-    modules: {}
+    mutations: {
+        // 方法
+
+
+        ['INCREMENT'](state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        incrementCount(state, count) {
+            state.counter += count
+        },
+        addStudent(state, stu) {
+            state.students.push(stu)
+        },
+        updateInfo(state) {
+            state.info.name = '111'
+
+            // 错误的代码：不能在这里进行异步操作
+            // setTimeout(() => {
+            //         state.info.name = '111'
+            //     }, 1000)
+
+
+            // state.info['address'] = '洛杉矶'
+            // Vue.set(state.info, 'address', 'Luoshanji')
+
+            // delete state.info.age
+            // Vue.delete(state.info, 'age')
+        }
+    },
+    actions: {
+        // context 上下文 即 store
+        // 方法一
+        // aUpdateInfo(context, payload) {
+        //     setTimeout(() => {
+        //         context.commit('updateInfo')
+        //         console.log(payload.message);
+        //         payload.success()
+        //     }, 1000)
+        // }
+
+        // 方法二
+        aUpdateInfo(context, payload) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    context.commit('updateInfo')
+                    console.log(payload);
+                    resolve('111')
+                }, 1000)
+            })
+        }
+    },
+    getters: {
+        powerCounter(state) {
+            return state.counter * state.counter
+        },
+        more20stu(state) {
+            return state.students.filter(s => s.age > 20)
+        },
+        more20stuLength(state, getters) {
+            return getters.more20stu.length
+        },
+        moreAgeStu(state) {
+            // return function(age) {
+            //     return state.students.filter(s => s.age > age)
+            // }
+            return age => {
+                return state.students.filter(s => s.age > age)
+            }
+        }
+    },
+    modules: {
+        // 按照模块划分
+        a: moduleA
+    }
 })
