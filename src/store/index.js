6ce2f8b19c6015cf98a8b5ff70215e91fc5e3cf5
guxiaona <15262526680@139.com>
2021-07-20 10:35:21 +0800

    第不知道几次修改

--- src/store/index.js
+++ src/store/index.js
@@ -43,6 +43,14 @@ const obj = {
     height: 1.88
 }
 const { name, age, height } = obj
-console.log(name);
-console.log(age);
-console.log(height);
+// console.log(name);
+// console.log(age);
+// console.log(height);
+
+// 数组解构
+const names = ['why', 'kobe', 'james']
+    // const name1=names[0]
+    // const name2=names[1]
+    // const name3=names[2]
+
+const [name1, name2, name3] = names;
